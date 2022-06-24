cf4e1f1da4882f8b1f8feb03127992844e2960c6
sirvvy <henry.wills.dev@gmail.com>
2022-06-24 22:39:10 +0100

    second commit

--- intro.js
+++ intro.js
@@ -10,11 +10,11 @@ if(isNaN(number1) || isNaN(number2)){
     if(operator === '+') {
         result = number1 + number2;
     }else if(operator === '-') {
-        result = number1 + number2;
+        result = number1 - number2;
     }else if(operator === '*') {
-        result = number1 + number2;
-    } else if(operator === '*') {
-        result = number1 + number2;
+        result = number1 * number2;
+    } else if(operator === '/') {
+        result = number1 / number2;
     }
     alert(number1+operator+number2+'=' + result);
 }
